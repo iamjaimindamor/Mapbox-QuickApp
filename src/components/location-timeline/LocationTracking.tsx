42d5226840e20633cb3d3f67b2272e7513fcaeb4
ALPSINFO\Jaimin.Damor <Jaimin.Damor@alpsinfo.net>
2024-12-03 10:25:48 +0530

    mapbox proj init

---
+++ src/components/location-timeline/LocationTracking.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SideBar from "../dashboard/SideBar";
+
+const LocationTracking = () => {
+  return (
+    <>
+      <SideBar />
+      <div className="col-md-11 welcome_div_mobile">
+        LocationTracking
+      </div>
+    </>
+  );
+};
+
+export default LocationTracking;
